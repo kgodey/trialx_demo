c34d60f4dd1a7420dbabf0e159ac5e4ce42909fd
Kriti Godey <kriti@kritigodey.com>
2013-12-12 19:04:23 -0500

    Modified function to get all the matches in two requests due to an API quirk

--- trialx/api.py
+++ trialx/api.py
@@ -2,10 +2,8 @@ import requests
 
 TRIALX_API_URL = 'http://trialx.com/query'
 
-def find_trial_matches(**kwargs):
+def find_all_trial_matches(**kwargs):
 	search_params = kwargs
 	search_params['op'] = 'json'
-	print search_params
-	trial_matches = requests.get(TRIALX_API_URL, params=search_params)
-	print trial_matches.url
-	return trial_matches.json()
+	search_params['start'] = requests.get(TRIALX_API_URL, params=search_params).json()['total']
+	return requests.get(TRIALX_API_URL, params=search_params).json()
