633866c850e6c73eadc8225beb35f5ed488a95e9
Kriti Godey <kriti@kritigodey.com>
2013-12-14 00:11:10 -0500

    updated models to reflect possible blank values

--- trialx/models.py
+++ trialx/models.py
@@ -5,20 +5,21 @@ class ClinicalTrial(models.Model):
 	STUDY_TYPE_CHOICES = (
 		('O', 'Observational'),
 		('I', 'Interventional'),
+		('U', 'Unknown')
 	)
 	title = models.TextField()
 	url = models.URLField()
-	source = models.TextField()
+	source = models.TextField(blank=True)
 	trialx_id = models.IntegerField()
-	phase = models.CharField(max_length=1)
-	study_type = models.CharField(max_length=1, choices=STUDY_TYPE_CHOICES)
-	condition = models.TextField()
+	phase = models.CharField(max_length=1, blank=True)
+	study_type = models.CharField(max_length=1, choices=STUDY_TYPE_CHOICES, default='U')
+	condition = models.TextField(blank=True)
 
 
 class ClinicalTrialSite(models.Model):
 	sitename = models.TextField()
- 	address = models.TextField()
-	city = models.CharField(max_length=255)
-	state = models.CharField(max_length=255)
-	zipcode = models.IntegerField(max_length=5)
+ 	address = models.TextField(blank=True)
+	city = models.CharField(max_length=255, blank=True)
+	state = models.CharField(max_length=255, blank=True)
+	zipcode = models.IntegerField(max_length=5, blank=True)
 	trial = models.ForeignKey(ClinicalTrial)
